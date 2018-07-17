9434dc36fb510f4d402613fc019245e137c6df31
bastlenoob <bastien.labouche96@orange.fr>
2018-07-17 11:11:15 +0200

    Ajout copie profil gestionnaire

--- crt.sh
+++ crt.sh
@@ -4,12 +4,14 @@ chroot squashfs
 wget https://github.com/ersplus/solibuntu/archive/Dev.zip
 unzip Dev.zip
 cp solibuntu-Dev/share/skel_admin.tar.gz /etc/
+cp solibuntu-Dev/share/skel_gest.tar.gz /etc/
 rm Dev.zip
 rm -rf solibuntu-Dev
 cd /etc/
 tar -xvzf skel_admin.tar.gz
+tar -xvzf skel_gest.tar.gz
 rm skel_admin.tar.gz
-
+rm skel_gest.tar.gz
 cd
 
 useradd -m administrateur -s /bin/bash -k /etc/skel_admin
@@ -17,7 +19,7 @@ echo -e "AdminSolibuntu\nAdminSolibuntu" | passwd administrateur
 usermod -c "Administrateur Solibuntu" administrateur
 adduser administrateur sudo
 
-useradd -m gestionnaire -s /bin/bash
+useradd -m gestionnaire -s /bin/bash -k /etc/skel_gest
 echo -e "AdminAsso\nAdminAsso" | passwd gestionnaire
 usermod -c "Gestionnaire Solibuntu" gestionnaire
 
