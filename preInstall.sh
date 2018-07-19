d8f3e39b046e27432c01f4ecdf2b32b7c29cf59b
bastlenoob <bastien.labouche96@orange.fr>
2018-07-19 13:14:57 +0200

    Modification des messages de pre-installation

--- preInstall.sh
+++ preInstall.sh
@@ -1,12 +1,12 @@
 zenity --info --width=500 --height=75 --text="Attention - Message Important
 
-Si vous poursuivez cette installation toutes les données de cet ordinateurs seront
-supprimées afin de pouvoir installer Solibuntu.
-Si vous désirez annuler cette installation veuillez éteindre votre ordinateur et
-retirer la clé immédiatement.."
+Si vous poursuivez cette installation toutes les données de cet ordinateurs seront\
+supprimées afin de pouvoir installer Solibuntu.\
+Si vous désirez annuler cette installation veuillez éteindre votre ordinateur et\
+retirer la clé immédiatement.."\
 
 zenity --info --width=500 --height=50 --text="Validation des licences
 
-En choisissant de poursuivre l'installation vous reconnaissez accepter la licence 
-d'utilisation des polices de caractères Microsoft disponible à cette adresse :
+En choisissant de poursuivre l'installation vous reconnaissez accepter la licence \
+d'utilisation des polices de caractères Microsoft disponible à cette adresse :\
 http://corefonts.sourceforge.net/eula.htm"
