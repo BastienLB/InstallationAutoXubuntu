22975fbc7953676a84504a486fabac71ce7e17cb
bastlenoob <bastien.labouche96@orange.fr>
2018-07-20 15:10:58 +0200

    Ajout information installation

--- preInstall.sh
+++ preInstall.sh
@@ -10,3 +10,5 @@ zenity --info --width=500 --height=50 --text="Validation des licences
 En choisissant de poursuivre l'installation vous reconnaissez accepter la licence \
 d'utilisation des polices de caractères Microsoft disponible à cette adresse :\
 http://corefonts.sourceforge.net/eula.htm"
+
+zenity --info --width=200 --text "L'installation de Solibuntu est en cours, veuillez patienter..."
